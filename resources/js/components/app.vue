c7f57da24506e1d6e5e942038e5458a09f6f74ec
chadnitalukder2 <chadnitalukder2@gmail.com>
2024-12-31 20:18:25 +0600

    first commit

---
+++ resources/js/components/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Hello App !</h1>
+    <p>
+        <strong>Current route path: </strong> {{ $route.fullPath }}
+    </p>
+    <nav>
+        <router-link class="link" to="/">Home</router-link>
+        <router-link class="link" to="/about">About</router-link>
+    </nav>
+    <main>
+        <router-view/>
+    </main>
+</template>
+<script>
+</script>
