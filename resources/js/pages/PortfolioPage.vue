12a4854b275c4607006420e3b23905d04be00dbd
chadnitalukder2 <chadnitalukder2@gmail.com>
2025-01-17 11:28:26 +0600

    responsive portfolio

--- resources/js/pages/PortfolioPage.vue
+++ resources/js/pages/PortfolioPage.vue
@@ -170,6 +170,16 @@
         display: grid;
         gap: 8px;
         grid-template-columns: repeat(4, 1fr);
+        @media (max-width: 1277px) {
+            grid-template-columns: repeat(3, 1fr);
+        }
+        @media (max-width: 702px) {
+            grid-template-columns: repeat(2, 1fr);
+        }
+        @media (max-width: 438px) {
+            grid-template-columns: repeat(1, 1fr);
+            gap: 20px;
+        }
 
         .item {
             a {
